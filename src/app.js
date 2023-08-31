d6e758b8cdc4c6f4c254558e926d3884716e296a
liotto marcon <viniciusheckert14@gmail.com>
2023-08-31 09:00:33 -0300

    cache de dados para natura

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
+// Bibliotecas
+import 'dotenv/config'
 import express from 'express'
+import NodeCache from 'node-cache'
+import cors from 'cors'
+// Projeto
 import db from './config/db.js'
 import sugestao from './routes/sugestaoRoutes.js'
 import elogios from './routes/elogioRoutes.js'
 import criticas from './routes/criticaRoutes.js'
 import usuarios from './routes/usuarioRoutes.js'
 import cardapios from './routes/cardapioRoutes.js'
-import cors from 'cors'
 import roles from './routes/rolesRoutes.js'
-import 'dotenv/config'
 import erroSolicitacao from './middleware/erroSolicitacao.js'
 
 db.on("error", console.log.bind(console, "Erro de conexão"))
 db.once("open", () =>  console.log("Sucesso ao conectar com o Banco de Dados"))
 
 const app = express()
+export const cache = new NodeCache()
 
 app.use(
     cors(),
--- src/controller/cardapioController.js
+++ src/controller/cardapioController.js
@@ -1,13 +1,23 @@
+import { cache } from "../app.js";
 import cardapios from "../models/Cardapio.js";
 
 class cardapioController {
 
     static listagemCardapios = async (req, res, next) => {
         try {
+            const cacheKey = 'cardapios';
+            const cacheData = cache.get(cacheKey)
+
+            if (cacheData) {
+                return res.status(200).send(cacheData)
+            }
+
             const dados = await cardapios.find();
             if (!dados) {
                 return res.status(404).send({message: "Dados não encontrados!"})
             }
+
+            cache.set(cacheKey, dados, 3600)
             return res.status(200).json(dados)
         } catch (err) {
             return next(err, req, res)
